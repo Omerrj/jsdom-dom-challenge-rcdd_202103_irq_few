f8f2e4896c48467b2fcda9a15adb76cc3171aee5
Omer Sabah <omerrj99@gmail.com>
2021-04-20 18:06:50 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -45,6 +45,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
     form.onsubmit=(e)=>{
       e.preventDefault()
-      list.append(`${comment.value}`)
+      list.append(`<p>${comment.value}</p>`)
     }
 });
